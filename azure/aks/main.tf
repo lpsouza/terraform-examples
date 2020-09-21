f566f414d3cd89491ca415079031d190340dcca9
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2020-09-21 11:44:04 -0300

    Initial commit

---
+++ azure/aks/main.tf
@@ -0,0 +1,62 @@
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+locals {
+  cluster_name   = "TestAKS"
+  vm_size        = "Standard_D2_v2"
+  admin_username = "lpsouza"
+  public_key     = tls_private_key.ssh.public_key_openssh
+  location       = "westus"
+  agent_count    = 1
+  dns_prefix     = "k8stest"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.cluster_name}RG"
+  location = local.location
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = local.cluster_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = local.location
+  dns_prefix          = local.dns_prefix
+
+  linux_profile {
+    admin_username = local.admin_username
+
+    ssh_key {
+      key_data = local.public_key
+    }
+  }
+
+  default_node_pool {
+    name       = "${lower(local.cluster_name)}pool"
+    node_count = local.agent_count
+    vm_size    = local.vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    load_balancer_sku = "Standard"
+    network_plugin    = "kubenet"
+  }
+}
+
+output "tls_private_key" {
+  value = "${tls_private_key.ssh.private_key_pem}"
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
---
+++ azure/virtual-machine/main.tf
@@ -0,0 +1,133 @@
+provider "azurerm" {
+  version = "~>2.0"
+  features {}
+}
+
+resource "tls_private_key" "ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "random_id" "randomId" {
+  keepers = {
+    resource_group = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+locals {
+  vm_name        = "TestVM"
+  vm_size        = "Standard_D2_v2"
+  admin_username = "lpsouza"
+  public_key     = tls_private_key.ssh.public_key_openssh
+  location       = "westus"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.vm_name}-RG"
+  location = local.location
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${local.vm_name}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = local.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${local.vm_name}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                = "${local.vm_name}-pip"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "${local.vm_name}-nsg"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "default-allow-ssh"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${local.vm_name}-nic"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${local.vm_name}-nic-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic-nsg-bound" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "diag${random_id.randomId.hex}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                  = local.vm_name
+  location              = local.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  size                  = local.vm_size
+
+  os_disk {
+    name                 = "${local.vm_name}-osdisk"
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18_04-lts-gen2"
+    version   = "latest"
+  }
+
+  computer_name                   = lower(local.vm_name)
+  admin_username                  = local.admin_username
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = local.admin_username
+    public_key = local.public_key
+  }
+
+  boot_diagnostics {
+    storage_account_uri = azurerm_storage_account.storage.primary_blob_endpoint
+  }
+}
+
+output "tls_private_key" {
+  value = "${tls_private_key.ssh.private_key_pem}"
+}
