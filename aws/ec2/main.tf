66c63cf1a4f5abd7823c95af359227855a8a43d8
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2023-12-22 16:40:42 -0300

    Add egress traffic to AWS EC2

--- aws/ec2/main.tf
+++ aws/ec2/main.tf
@@ -53,6 +53,14 @@ resource "aws_security_group" "my_security_group" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "my_instance" {
