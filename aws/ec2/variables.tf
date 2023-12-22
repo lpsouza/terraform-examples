bd24c7cb72d10ec1936d0dd50109817220420908
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2023-12-22 10:24:22 -0300

    Added default (example) values to AWS EC2 variables

--- aws/ec2/variables.tf
+++ aws/ec2/variables.tf
@@ -1,19 +1,23 @@
 variable "AWS_REGION" {
   description = "AWS region"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "AWS_ACCESS_KEY" {
   description = "AWS access key"
   type        = string
+  default     = "ASDFASDFASDFASDFASDF"
 }
 
 variable "AWS_SECRET_KEY" {
   description = "AWS secret key"
   type        = string
+  default     = "ASDFASDFASDFASDFASDFASDFASDFASDFASDFASDF"
 }
 
 variable "AWS_KEY_PAIR" {
   description = "AWS key pair"
   type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAA... user@host"
 }
