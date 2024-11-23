ca475ff51c3aaa3c45ddaa65472aae9ec62d95fe
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2024-11-23 18:48:18 -0300

    Add S3 bucket configuration and lifecycle rules

---
+++ cloud/aws/s3/main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region     = var.AWS_REGION
+  access_key = var.AWS_ACCESS_KEY
+  secret_key = var.AWS_SECRET_KEY
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.S3_BUCKET_NAME
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "my_bucket_lifecycle" {
+  bucket = aws_s3_bucket.my_bucket.id
+
+  rule {
+    id     = "MoveToInfrequentAccess"
+    status = "Enabled"
+
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+  }
+
+  rule {
+    id     = "MoveToGlacier"
+    status = "Enabled"
+
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+}
---
+++ cloud/aws/s3/variables.tf
@@ -0,0 +1,23 @@
+variable "AWS_REGION" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "AWS_ACCESS_KEY" {
+  description = "AWS access key"
+  type        = string
+  default     = "ASDFASDFASDFASDFASDF"
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "AWS secret key"
+  type        = string
+  default     = "ASDFASDFASDFASDFASDFASDFASDFASDFASDFASDF"
+}
+
+variable "S3_BUCKET_NAME" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "my-bucket-example-123"
+}
